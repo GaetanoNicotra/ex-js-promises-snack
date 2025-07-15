e56e36d900d9ec5bedc1940c8065383da321acfb
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-07-15 18:55:50 +0200

    [FIX]

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function getPostTitle(id) {
     const promessa = new Promise((resolve, reject) => {
         fetch(`https://dummyjson.com/posts/${id}`)
             .then(response => response.json())
-            .then(obj => resolve(obj))
+            .then(obj => resolve(obj.title))
             .catch(reject)
     })
     return promessa
 }
 
-getPostTitle(26).then(obj => console.log(obj))
+getPostTitle(6).then(obj => console.log('Titolo del post: ' + '' + obj))
     .catch(error => console.error(error))
