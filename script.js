b060a23001f518a52f9dd6cf1fa1298bf2d9f079
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-07-16 18:13:01 +0200

    [UPD] snack 2 e 2-bonus completati

--- script.js
+++ script.js
@@ -24,8 +24,11 @@ function getPost(id) {
                 fetch(`https://dummyjson.com/users/${postData.userId}`)
                     .then(response => response.json())
                     .then(userData => {
-                        postData.user = userData;
-                        resolve(postData);
+                        const result = {
+                            ...postData,
+                            userData,
+                        }
+                        resolve(result)
                     })
                     .catch(reject)
             )
@@ -38,3 +41,60 @@ function getPost(id) {
 getPost(12)
     .then(post => console.log('Post:', post))
     .catch(error => console.error('Errore:', error));
+
+
+// SANCK 2
+
+function lanciaDado() {
+    const promessa = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const incastrato = Math.random() < 0.2
+            if (incastrato) {
+                reject("Ops.. il dado si è incastrato")
+
+            } else {
+                const risultato = Math.floor(Math.random() * 6) + 1;
+                resolve(risultato)
+            }
+        }, 3000)
+    })
+    return promessa
+}
+
+lanciaDado().then(risultato => console.log('Il dado ha lanciato' + ' ' + risultato))
+    .catch(err => console.error(err));
+
+
+// SNACK 2 BONUS
+
+const creaLanciaDado = () => {
+    let ultimoLancio = null;
+    return function () {
+        const promessa = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (Math.random() < 0.2) {
+                    ultimoLancio = null;
+                    reject("Ops.. il dado si è incastrato")
+                } else {
+                    const risultato = Math.floor(Math.random() * 6) + 1;
+                    if (risultato === ultimoLancio) {
+                        console.log('INCREDIBILE')
+                    }
+                    ultimoLancio = risultato;
+                    resolve(risultato)
+                }
+            }, 1000)
+        })
+        return promessa
+    }
+}
+
+const lanciaDado2 = creaLanciaDado()
+
+lanciaDado2()
+    .then(risultato => console.log('Il dado2 ha lanciato' + ' ' + risultato))
+    .catch(err => console.error(err));
+
+lanciaDado2()
+    .then(risultato => console.log('Il dado2 ha lanciato' + ' ' + risultato))
+    .catch(err => console.error(err));
