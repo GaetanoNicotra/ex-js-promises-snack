31c1809fd75805554a63c530d61faedf6155a97d
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-07-16 13:11:44 +0200

    [UPD] snack 1 bonus completato

--- script.js
+++ script.js
@@ -11,4 +11,30 @@ function getPostTitle(id) {
 }
 
 getPostTitle(6).then(obj => console.log('Titolo del post: ' + '' + obj))
-    .catch(error => console.error(error))
+    .catch(error => console.error(error));
+
+
+// SNACK 1 BONUS
+
+function getPost(id) {
+    const promessa = new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(response => response.json())
+            .then(postData =>
+                fetch(`https://dummyjson.com/users/${postData.userId}`)
+                    .then(response => response.json())
+                    .then(userData => {
+                        postData.user = userData;
+                        resolve(postData);
+                    })
+                    .catch(reject)
+            )
+            .catch(reject);
+    });
+
+    return promessa;
+}
+
+getPost(12)
+    .then(post => console.log('Post:', post))
+    .catch(error => console.error('Errore:', error));
